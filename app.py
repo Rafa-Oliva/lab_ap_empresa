63fe9bc5445f991cdb7c9676bbbf95fb691981eb
Rafael Oliva <rafaoliva98@uma.es>
2022-12-16 00:51:04 +0100

    Añadidas las funcionalidades del bloque del navegador: login, register e incognito

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, escape
+from flask import Flask, render_template, request, escape, redirect, url_for
 from search4web import search4letters, log_request
-import sqlite_db
+from sqlite_db import*
 
 app = Flask(__name__)
 
@@ -18,6 +18,13 @@ app = Flask(__name__)
 # def index():
 #    return f"Index Page"
 
+
+
+#C:\Users\rafao\Documents\2_Master\Laboratorio_Aplicaciones_Empresariales\myproject>py -m flask run
+
+#delete_user_database()
+start_user_database()
+
 @app.route("/search", methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
@@ -27,15 +34,45 @@ def do_search() -> 'html':
     log_request(request, result)
     return str(search4letters(phrase, letters))
 
-@app.route("/login")
+@app.route("/login", methods=['POST'])
 def login_page():
-    return render_template('login.html')
+    if request.form.get('login') == 'LOGIN':
+        username = request.form['username']
+        password = request.form['password']
+        if login_authentication(username,password):
+            return redirect(url_for('entry_page', username=username))
+        else:
+            return render_template('display_error.html', the_error='Incorrect username and/or password')
+    elif request.form.get('register') == 'REGISTER':
+        redirect(url_for('register_page'))
+        return render_template('register.html')
+    elif request.form.get('incognito_mode') == 'INCOGNITO_MODE':
+        login_anonymous()
+        return redirect(url_for('entry_page', username='Anonymous'))
+
+@app.route("/register", methods=['POST'])
+def register_page():
+    if request.form.get('register') == 'REGISTER':
+        username = request.form['username']
+        password = request.form['password']
+        if register_new_user(username,password):
+            return redirect(url_for('entry_page', username=username))
+        else:
+            return render_template('display_error.html', the_error='The username selected already exists')
+
 
 @app.route('/')
-@app.route("/entry")
-def entry_page():
-    return render_template('entry.html',
-                           the_title='Welcome to search  letters on the web!')
+#@app.route("/index")
+def main_page():
+    return render_template('login.html')
+
+@app.route("/<username>/entry")
+def entry_page(username):
+    return render_template('entry.html')
+
+@app.route("/error")
+def error_page(error):
+    return render_template('display_error.html', the_error=error)
 
 
 @app.route("/result", methods=['POST'])
--- sqlite_db.py
+++ sqlite_db.py
@@ -1,105 +1,110 @@
 import sqlite3, datetime
 
-db = 'userpool.db'   #Data base name
+db = 'userpool.db'  # Data base name
+
 
 def start_user_database():
-   connection = sqlite3.connect(db)
-   cursor = connection.cursor()
+    connection = sqlite3.connect(db)
+    cursor = connection.cursor()
 
-   cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-      name VARCHAR(50) PRIMARY KEY UNIQUE NOT NULL,
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+      username VARCHAR(50) PRIMARY KEY UNIQUE NOT NULL,
       password TEXT NOT NULL )''')
 
-   cursor.execute('''CREATE TABLE IF NOT EXISTS log (
-      name VARCHAR(50) NOT NULL,
+    cursor.execute('''CREATE TABLE IF NOT EXISTS log (
+      username VARCHAR(50) NOT NULL,
       action VARCHAR(100) NOT NULL,
       time DATATIME NOT NULL,
-      FOREIGN KEY(name) REFERENCES usuarios(name))''')
+      FOREIGN KEY(username) REFERENCES users(username))''')
 
-   cursor.execute('''CREATE TABLE IF NOT EXISTS visit_counter (
-      name VARCHAR(50) PRIMARY KEY UNIQUE NOT NULL,
+    cursor.execute('''CREATE TABLE IF NOT EXISTS visit_counter (
+      username VARCHAR(50) PRIMARY KEY UNIQUE NOT NULL,
       number_visits INTEGER DEFAULT 1,
-      FOREIGN KEY(name) REFERENCES usuarios(name))''')
+      FOREIGN KEY(username) REFERENCES users(username))''')
 
-   cursor = connection.execute("SELECT name FROM users WHERE name=?", ('Anonymous'))
-   if not cursor.getchone():  # Anonymous already exists, no need to create this user
-      connection.execute("INSERT INTO users (name, password) VALUES (?, ?)", (name, '6666'));   #Anonymous password won't ever be used (but can't be null)
+    cursor = connection.execute("SELECT username FROM users WHERE username=?", ['Anonymous'])
+    if not cursor.fetchone():  # Anonymous already exists, no need to create this user
+        connection.execute("INSERT INTO users (username, password) VALUES (?, ?)",
+                           ('Anonymous', '6666'));  # Anonymous password won't ever be used (but can't be null)
 
-   connection.commit()
-   connection.close()
+    connection.commit()
+    connection.close()
 
-def login_authentication(name:str, password:str) -> bool:
-   connection = sqlite3.connect(db)
-   cursor = connection.cursor()
 
-   cursor = connection.execute("SELECT name, password FROM users WHERE name=? AND password=?", (name, password))
-   if cursor.getchone():   # Correct username and password, login successful
-      connection.execute("UPDATE visit_counter SET number_visits=number_visits+1 WHERE name=?", (name));
-      connection.execute("INSERT INTO log (name, action, time) VALUES (?, ?, ?)", (name, "Login successful", datetime.datetime.now()));
-      connection.commit()
-      login = True
-   else:                   # Wrong username and/or passsword, login failed
-      login = False
-   connection.close()
-   return login
+def login_authentication(username: str, password: str) -> bool:
+    connection = sqlite3.connect(db)
+    cursor = connection.cursor()
 
-def register_new_user(name:str, password:str) -> bool:
-   connection = sqlite3.connect(db)
-   cursor = connection.cursor()
+    cursor = connection.execute("SELECT username, password FROM users WHERE username=? AND password=?", (username, password))
+    if cursor.fetchone():  # Correct username and password, login successful
+        connection.execute("UPDATE visit_counter SET number_visits=number_visits+1 WHERE username=?", [username]);
+        connection.execute("INSERT INTO log (username, action, time) VALUES (?, ?, ?)", (username, "Login successful", datetime.datetime.now()));
+        connection.commit()
+        login = True
+    else:  # Wrong username and/or passsword, login failed
+        login = False
+    connection.close()
+    return login
 
-   cursor = connection.execute("SELECT name, password FROM users WHERE name=?", (name))
-   if cursor.getchone():  # Username already exists, can't register
-      registration = False
-   else:
-      connection.execute("INSERT INTO users (name, password) VALUES (?, ?)", (name, password));
-      connection.execute("INSERT INTO visit_counter (name) VALUES (?)", (name));
-      connection.execute("INSERT INTO log (name, action, time) VALUES (?, ?, ?)", (name, "Registration successful", datetime.datetime.now()));
-      connection.commit()
-      registration = True
-   connection.close()
-   return registration
 
-def new_entry_log(name:str, action:str):
-   connection = sqlite3.connect(db)
-   cursor = connection.cursor()
-   connection.execute("INSERT INTO log (name, action, time) VALUES (?, ?, ?)", (name, action, datetime.datetime.now()));
-   connection.commit()
-   connection.close()
+def register_new_user(username: str, password: str) -> bool:
+    connection = sqlite3.connect(db)
+    cursor = connection.cursor()
+
+    cursor = connection.execute("SELECT username, password FROM users WHERE username=?", [username])
+    if cursor.fetchone():  # Username already exists, can't register
+        registration = False
+    else:
+        connection.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password));
+        connection.execute("INSERT INTO visit_counter (username) VALUES (?)", [username]);
+        connection.execute("INSERT INTO log (username, action, time) VALUES (?, ?, ?)", (username, "Registration successful", datetime.datetime.now()));
+        connection.commit()
+        registration = True
+    connection.close()
+    return registration
+
+
+def new_entry_log(username: str, action: str):
+    connection = sqlite3.connect(db)
+    cursor = connection.cursor()
+    connection.execute("INSERT INTO log (username, action, time) VALUES (?, ?, ?)", (username, action, datetime.datetime.now()));
+    connection.commit()
+    connection.close()
+
 
 def login_anonymous():
-   connection = sqlite3.connect(db)
-   cursor = connection.cursor()
+    connection = sqlite3.connect(db)
+    cursor = connection.cursor()
 
-   cursor = connection.execute("SELECT name FROM visit_counter WHERE name=?", ('Anonymous'))
-   if cursor.getchone():   # Anonymous visit counter exists, need to update it (+1)
-      connection.execute("UPDATE visit_counter SET number_visits=number_visits+1 WHERE name=?", (name))
-   else:                   # Anonymous visit counter doesn't exists, need to create it
-      connection.execute("INSERT INTO visit_counter (name) VALUES (?)", ('Anonymous'))
+    cursor = connection.execute("SELECT username FROM visit_counter WHERE username=?", ['Anonymous'])
+    if cursor.fetchone():  # Anonymous visit counter exists, need to update it (+1)
+        connection.execute("UPDATE visit_counter SET number_visits=number_visits+1 WHERE username=?", ['Anonymous'])
+    else:  # Anonymous visit counter does not exist, need to create it
+        connection.execute("INSERT INTO visit_counter (username) VALUES (?)", ['Anonymous'])
 
-   connection.execute("INSERT INTO log (name, action, password) VALUES (?, ?, ?)", ('Anonymous', "Login successful", datetime.datetime.now()))
-   connection.commit()
-   connection.close()
-
-
-def delete_user_database():                              #Only delete IF they already exist => avoid errors
-   connection = sqlite3.connect(db)
-   cursor = connection.cursor()
-
-   cursor = connection.execute("SELECT name FROM userpool.db WHERE type='table' AND name=users")
-   if cursor.getchone():
-      cursor.execute("DROP TABLE users")
-   cursor = connection.execute("SELECT name FROM userpool.db WHERE type='table' AND name=log")
-   if cursor.getchone():
-      cursor.execute("DROP TABLE log")
-   cursor = connection.execute("SELECT name FROM userpool.db WHERE type='table' AND name=visit_counter")
-   if cursor.getchone():
-      cursor.execute("DROP TABLE visit_counter")
-
-   connection.commit()
-   connection.close()
+    connection.execute("INSERT INTO log (username, action, time) VALUES (?, ?, ?)", ('Anonymous', "Login successful", datetime.datetime.now()))
+    connection.commit()
+    connection.close()
 
 
-#cursor = connection.execute("SELECT name, password FROM users")
-#for row in cursor:
+def delete_user_database():  # Only delete IF they already exist => avoid errors
+    connection = sqlite3.connect(db)
+    cursor = connection.cursor()
+
+    cursor = connection.execute("SELECT name FROM userpool.db WHERE type='table' AND username=users")
+    if cursor.fetchone():
+        cursor.execute("DROP TABLE users")
+    cursor = connection.execute("SELECT name FROM userpool.db WHERE type='table' AND username=log")
+    if cursor.fetchone():
+        cursor.execute("DROP TABLE log")
+    cursor = connection.execute("SELECT name FROM userpool.db WHERE type='table' AND name=visit_counter")
+    if cursor.fetchone():
+        cursor.execute("DROP TABLE visit_counter")
+
+    connection.commit()
+    connection.close()
+
+# cursor = connection.execute("SELECT name, password FROM users")
+# for row in cursor:
 #   print("name = ", row[0])
 #   print("password = ", row[1], "\n")
